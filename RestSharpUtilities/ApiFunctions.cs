9ff350e059570b4678cb7656a154d81d33685b80
Jia Yu <yujiayujia9@hotmail.com>
2022-07-11 02:19:52 +1200

    create utility class library

---
+++ RestSharpUtilities/ApiFunctions.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharpUtilities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpUtilities
+{
+    public class ApiFunctions
+    {
+        private Helper helper;
+
+        public ApiFunctions()
+        {
+            helper = new Helper();
+        }
+        public async Task<RestResponse> GetUsers(string baseUrl)
+        {
+            //helper.AddCertificate("", "");
+            var client = helper.SetUrl(baseUrl, "api/users?page=2");
+            var request = helper.CreateGetRequest();
+            request.RequestFormat = DataFormat.Json;
+            var response = await helper.GetResponseAsync(client, request);
+            return response;
+        }
+
+        public async Task<RestResponse> CreateNewUser(string baseUrl, dynamic payload)
+        {
+            var client = helper.SetUrl(baseUrl, "api/users");
+            //var jsonString = HandleContent.SerializeJsonString(payload);
+            var request = helper.CreatePostRequest<CreateUserReq>(payload);
+            var response = await helper.GetResponseAsync(client, request);
+            return response;
+        }
+    }
+}
---
+++ RestSharpUtilities/HandleContent.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace RestSharpUtilities
+{
+    public static class HandleContent
+    {
+        public static T GetContent<T>(RestResponse response)
+        {
+            var content = response.Content;
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+
+        public static string SerializeJsonString(dynamic content)
+        {
+            return JsonConvert.SerializeObject(content, Formatting.Indented);
+        }
+
+        public static T ParseJson<T>(string file)
+        {
+            return JsonConvert.DeserializeObject<T>(File.ReadAllText(file));
+        }
+    }
+}
